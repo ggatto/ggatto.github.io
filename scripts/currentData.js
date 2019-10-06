923e52a5259f54f0351f3eb3613dced86ff7c768
Gilmar Gatto <gatto@byui.edu>
2019-10-05 19:00:29 -0600

    update

--- scripts/currentData.js
+++ scripts/currentData.js
@@ -2,3 +2,5 @@ var currentDate = new Date();
 var currentYear = currentDate.getFullYear();
 
 document.getElementById("currentYear").innerHTML = currentYear;
+document.getElementById("updatedOn").innerHTML =
+  "Updated on: " + document.lastModified.split(" ")[0];
