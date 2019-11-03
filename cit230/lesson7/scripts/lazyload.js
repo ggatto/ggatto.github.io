82965df5aaebdd65c7c7f55f11ae92f57373f34c
Gilmar Gatto <gatto@byui.edu>
2019-11-02 21:33:14 -0600

    JS change

--- cit230/lesson7/scripts/lazyload.js
+++ cit230/lesson7/scripts/lazyload.js
@@ -1,17 +1,10 @@
 const imagesToLoad = document.querySelectorAll('img[data-src]');
-
-const imgOption = {
-    threshold: 0,
-    rootMargin: "0px 0px 0px 0px"
-};
-
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');
     };
 };
-
 if ('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
